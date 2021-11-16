6aeb50fdb1342d0c1f27ad34bb48f8f3b2735956
login101i <maciejkruszyniak@gmail.com>
2021-11-16 18:24:16 +0100

    rn start template

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+
+import {
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	View,
+	Platform,
+	StatusBar,
+} from "react-native";
+
+import { RestaurantScreen } from "./src/features/restaurants/screens/RestaurantScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<RestaurantScreen />
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
