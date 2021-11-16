afb584c33f3537a42df4b376f468ab67d1f4bca5
login101i <maciejkruszyniak@gmail.com>
2021-11-16 20:36:31 +0100

    theme

---
+++ src/infrasctructure/theme/images.js
@@ -0,0 +1,11 @@
+const banner = require("../../assets/images/banner.png");
+const bitcoin = require("../../assets/images/bitcoin.png");
+const ethereum = require("../../assets/images/ethereum.png");
+const litecoin = require("../../assets/images/litecoin.png");
+
+export default {
+  banner,
+  bitcoin,
+  ethereum,
+  litecoin,
+};
---
+++ src/infrasctructure/theme/index.js
@@ -0,0 +1,6 @@
+import dummyData from "./dummy";
+import icons from "./icons";
+import images from "./images";
+import theme from "./theme";
+
+export { dummyData, icons, images, theme };
