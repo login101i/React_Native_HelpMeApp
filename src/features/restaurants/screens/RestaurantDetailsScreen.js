e0a8d838dcbd2f49092cc1309e4b82f703817a0a
login101i <maciejkruszyniak@gmail.com>
2021-11-24 21:26:52 +0100

    RestaurantDetails Accordions

--- src/features/restaurants/screens/RestaurantDetailsScreen.js
+++ src/features/restaurants/screens/RestaurantDetailsScreen.js
@@ -1,12 +1,58 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+
+import { List } from "react-native-paper";
+import { ScrollView } from "react-native";
+
+import { RestaurantInfoCard } from "../components/RestaurantInfoCard";
+import { SafeArea } from "../../../components/SafeArea";
 
 export const RestaurantDetailsScreen = ({ route }) => {
 	const { restaurant } = route.params;
+
+	const [expanded, setExpanded] = useState(false);
+
+	const handlePress = () => setExpanded(!expanded);
+
 	return (
-		<View>
-			<Text>{restaurant.address}</Text>
-		</View>
+		<SafeArea>
+			<RestaurantInfoCard restaurant={restaurant} />
+			<ScrollView>
+				<List.Accordion
+					title="Breakfast"
+					left={(props) => <List.Icon {...props} icon="bread-slice" />}
+				>
+					<List.Item title="First item" />
+					<List.Item title="Second item" />
+				</List.Accordion>
+
+				<List.Accordion
+					title="Dinners"
+					left={(props) => (
+						<List.Icon {...props} icon="silverware-fork-knife" />
+					)}
+					expanded={expanded}
+					onPress={handlePress}
+				>
+					<List.Item title="First item" />
+					<List.Item title="Second item" />
+					<List.Item title="First item" />
+					<List.Item title="Second item" />
+					<List.Item title="First item" />
+					<List.Item title="Second item" />
+				</List.Accordion>
+				<List.Accordion
+					title="Lunches"
+					left={(props) => <List.Icon {...props} icon="food-fork-drink" />}
+					expanded={expanded}
+					onPress={handlePress}
+				>
+					<List.Item title="First item" />
+
+					<List.Item title="First item" />
+					<List.Item title="Second item" />
+				</List.Accordion>
+			</ScrollView>
+		</SafeArea>
 	);
 };
 
