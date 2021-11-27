b3b91870b8d3105147fdabb4af9afbb4bc62d67a
Maciej <maciejkruszyniak@gmail.com>
2021-11-27 17:14:37 +0100

    firebase base setup

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import * as firebase from 'firebase'
 
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrasctructure/theme";
@@ -15,6 +16,38 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+	const [isAuthenticaded, setIsAuthenticaded] = useState(false)
+
+
+	useEffect(() => {
+		setTimeout(() => {
+			firebase.auth()
+				.signInWithEmailAndPassword("mk@wp.pl, 123456")
+				.then((user) => {
+					setIsAuthenticaded(true)
+				})
+				.catch((e) => {
+					console.log(e)
+				})
+		}, 4000)
+
+	}, [])
+
+	let initializeApp
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyAUCCcmui6TkzDup6fv6uJkUY3xAY3W-Bo",
+		authDomain: "helpmeapp-205c4.firebaseapp.com",
+		projectId: "helpmeapp-205c4",
+		storageBucket: "helpmeapp-205c4.appspot.com",
+		messagingSenderId: "974486216033",
+		appId: "1:974486216033:web:e2050af62e5f63d7173920"
+	};
+
+	if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+	  }
+
 	const [oswaldLoaded] = useOswald({
 		Oswald_400Regular
 	});
@@ -26,6 +59,9 @@ export default function App() {
 	if (!oswaldLoaded || !latoLoaded) {
 		return null;
 	}
+
+	if (!isAuthenticaded) return null
+
 
 	return (
 		<>
